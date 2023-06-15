c4121f4cf4c1422118cd1373f34e66f5bed7fef8
sindamb <sindambiwesylvere2018@gmail.com>
2023-06-15 12:20:39 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - A program that prints asizes of different types
- * Return: Always 0 (success)
- */
+* main - A program that prints asizes of different types
+* Return: Always 0 (success)
+*/
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
-printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(b));
-printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
-printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(d));
-printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(f));
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
 

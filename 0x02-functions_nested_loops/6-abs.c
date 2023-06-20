6c98260ff44ec5f922c9b53d535125eb9641ebe8
sindamb <sindambiwesylvere2018@gmail.com>
2023-06-20 11:58:00 -0700

    abslute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Absolute value of a number
+ * @t: number
+ * Return: Always 0 (success)
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+	{
+		return (t);
+	}
+	else
+	{
+		return (-t);
+	}
+}

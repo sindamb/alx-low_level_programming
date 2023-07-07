2f685e389ba7dfde45733cf0979eea9b1b11c724
sindamb <sindambiwesylvere2018@gmail.com>
2023-07-07 03:28:24 -0700

    uppercase letters

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 
 	while (str[++index])
 	{
-		while ((str[index] >= 'a' && str[index] <= 'z'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||

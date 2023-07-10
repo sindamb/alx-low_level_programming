315fa6c1a212a9a2c890dbc67f0af47c9a0397ea
sindamb <sindambiwesylvere2018@gmail.com>
2023-07-10 08:08:19 -0700

    sylvere

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include "main.h"
 
+void _puts(char *s);
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    _putchar('"\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick"');
     return (0);
 }

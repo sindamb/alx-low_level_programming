9bc87d04a32ff0ba87e0de9730f1ad84909c585c
sindamb <sindambiwesylvere2018@gmail.com>
2023-07-11 00:58:53 -0700

    sylvere

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,2 +1,14 @@
-#!/bin/bash
+#include "main.h"
+/**
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
 int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+
+	return (0);
+}
